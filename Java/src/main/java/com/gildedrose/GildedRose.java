40fb2b8f52be89aaa830809d0a4a5c0bbf7732ee
Andy Palmer <andy@andypalmer.com>
2018-09-14 18:37:54 +1000

    A WrappedItem handles its own quality calculations (and we rely on mutating the underlying Item to keep the code working... thanks Mr Goblin)

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,53 +1,21 @@
 package com.gildedrose;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 class GildedRose {
     Item[] items;
+    private final List<WrappedItem> wrappedItems;
 
     public GildedRose(Item[] items) {
         this.items = items;
+        wrappedItems = Stream.of(items).map(WrappedItem::new).collect(Collectors.toList());
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            Item item = items[i];
-
-            if(isLegendary(item)) { continue; }
-
-            item.sellIn = item.sellIn - 1;
-
-            if (degrading(item)) {
-                item.quality = item.quality - (!pastSellBy(item) ? 1 : 2);
-            } else {
-                item.quality = item.quality + (!pastSellBy(item) ? 1 : 2);
-                if (backstage(item)) {
-                    if (item.sellIn < 10) { item.quality = item.quality + 1; }
-                    if (item.sellIn < 5)  { item.quality = item.quality + 1; }
-                    if (pastSellBy(item)) { item.quality = 0; }
-                }
-            }
-            if (item.quality < 0)  { item.quality = 0;  }
-            if (item.quality > 50) { item.quality = 50; }
+        for(WrappedItem item : wrappedItems) {
+            item.updateQuality();
         }
     }
-
-    private boolean backstage(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private boolean pastSellBy(Item item) {
-        return item.sellIn < 0;
-    }
-
-    private boolean degrading(Item item) {
-        return !maturing(item) && !backstage(item);
-    }
-
-    private boolean maturing(Item item) {
-        return item.name.equals("Aged Brie");
-    }
-
-    private boolean isLegendary(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
 }
---
+++ Java/src/main/java/com/gildedrose/WrappedItem.java
@@ -0,0 +1,48 @@
+package com.gildedrose;
+
+public class WrappedItem {
+    private final Item item;
+
+    public WrappedItem(Item item) {
+        this.item = item;
+    }
+
+    public void updateQuality() {
+        if(isLegendary(item)) { return; }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (degrading(item)) {
+            item.quality = item.quality - (!pastSellBy(item) ? 1 : 2);
+        } else {
+            item.quality = item.quality + (!pastSellBy(item) ? 1 : 2);
+            if (backstage(item)) {
+                if (item.sellIn < 10) { item.quality = item.quality + 1; }
+                if (item.sellIn < 5)  { item.quality = item.quality + 1; }
+                if (pastSellBy(item)) { item.quality = 0; }
+            }
+        }
+        if (item.quality < 0)  { item.quality = 0;  }
+        if (item.quality > 50) { item.quality = 50; }
+    }
+
+    private boolean backstage(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean pastSellBy(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private boolean degrading(Item item) {
+        return !maturing(item) && !backstage(item);
+    }
+
+    private boolean maturing(Item item) {
+        return item.name.equals("Aged Brie");
+    }
+
+    private boolean isLegendary(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+}
