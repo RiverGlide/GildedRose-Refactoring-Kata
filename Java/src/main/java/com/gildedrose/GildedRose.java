52907363744fb75aa3f41c1420a0908a4014fef7
Andy Palmer <andy@andypalmer.com>
2018-09-14 18:35:41 +1000

    Work out the past sell-by quality at the same time as normal quality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,28 +13,16 @@ class GildedRose {
 
             if(isLegendary(item)) { continue; }
 
-            if (degrading(item)) {
-                item.quality = item.quality - 1;
-            } else {
-                item.quality = item.quality + 1;
-                if (backstage(item)) {
-                    if (item.sellIn < 11) { item.quality = item.quality + 1; }
-                    if (item.sellIn < 6)  { item.quality = item.quality + 1; }
-                }
-            }
-
             item.sellIn = item.sellIn - 1;
 
-            if (pastSellBy(item)) {
-                if(backstage(item)) {
-                    item.quality = 0;
-                    continue;
-                }
-
-                if (maturing(item)) {
-                    item.quality = item.quality + 1;
-                } else {
-                    item.quality = item.quality - 1;
+            if (degrading(item)) {
+                item.quality = item.quality - (!pastSellBy(item) ? 1 : 2);
+            } else {
+                item.quality = item.quality + (!pastSellBy(item) ? 1 : 2);
+                if (backstage(item)) {
+                    if (item.sellIn < 10) { item.quality = item.quality + 1; }
+                    if (item.sellIn < 5)  { item.quality = item.quality + 1; }
+                    if (pastSellBy(item)) { item.quality = 0; }
                 }
             }
             if (item.quality < 0)  { item.quality = 0;  }
